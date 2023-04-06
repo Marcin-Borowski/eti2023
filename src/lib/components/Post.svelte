14e30489d239c45f7d878dca1a3e0ce4f9899aa6
Marcin Borowski <mborowski@mindgroup.pl>
2023-04-06 09:37:27 +0200

    start

---
+++ src/lib/components/Post.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let title = ""
+  export let abstract = ""
+</script>
+
+<div class="p-4 text-2xl">
+  <h2 class="font-medium text-3xl my-2">{title}</h2>
+  <div class="bg-stone-50">
+    {@html abstract}
+  </div>
+  <slot>
+    
+  </slot>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Post from "$lib/components/Post.svelte";
+
+  let posts = [
+    {
+      title: "Curabitur est gravida et libero vitae dictum.",
+      abstract: "Salutantibus vitae elit libero, a pharetra augue. Pellentesque habitant morbi tristique senectus et netus. Tityre, tu patulae recubans sub tegmine fagi dolor.",
+      active: true,
+    },
+    {
+      title: "Excepteur sint obcaecat cupiditat non proident culpa.",
+      abstract: "Nihilne te <strong>nocturnum</strong> praesidium Palati, nihil urbis vigiliae. Non equidem invideo, miror magis posuere velit aliquet. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Ullamco laboris nisi ut aliquid ex ea commodi consequat. Nihil hic munitissimus habendi senatus locus, nihil horum? Nec dubitamus multa iter quae et nos invenerat.",
+      active: true,
+      keywords: "test, cos, eti"
+    },
+  ];
+
+  let titleInput;
+  let title = ""
+  let abstractInput;
+  let abstract = ""
+  
+
+    const onClick = () => {
+     posts = [...posts, {
+        title,
+        abstract,
+        active: true,
+        keywords: ""
+      }]
+
+      titleInput.value = ""
+      abstractInput.value = ""
+    }
+
+</script>
+
+<h1 class="text-4xl text-center text-sky-700">Mój BLOG</h1>
+
+<form class="grid grid-cols-1 gap-2 w-2/5 mx-auto my-8"> 
+  <input bind:this={titleInput} bind:value={title} placeholder="Tytuł" type="text" class="border border-violet-700 py-1 px-3">
+  <textarea bind:this={abstractInput} bind:value={abstract} placeholder="Treść" class="border border-violet-700 py-1 px-3"></textarea>
+  <button on:click={onClick} class="bg-violet-700 text-white px-3 py-1 hover:bg-violet-800">Dodaj</button>
+</form>
+
+
+<div class="grid grid-cols-2 gap-4">
+  {#each posts as item}
+    {#if item.active}
+    
+      <Post {...item}>
+        <div class="italic text-orange-600">{item.keywords}</div>
+      </Post>
+
+    {/if}
+  {/each}
+</div>
